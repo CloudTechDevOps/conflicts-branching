ce0b090dc7e3a814d58c8b84e91613091d49130c
veera <veerababu.narni232@gmail.com>
2025-09-06 09:16:38 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
       "Version": "2012-10-17",
       "Statement": [
         {
-          "Effect": "Allow",
+          "Effect": "deny",
           "Principal": {
             "Service": "eks.amazonaws.com"
           },
