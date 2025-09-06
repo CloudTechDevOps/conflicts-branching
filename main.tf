c9aacfebbdf76e5b8cb4d288990c5dcb692cb25e
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-09-06 09:19:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
       "Version": "2012-10-17",
       "Statement": [
         {
-          "Effect": "deny",
+          "Effect": "allow",
           "Principal": {
             "Service": "eks.amazonaws.com"
           },
